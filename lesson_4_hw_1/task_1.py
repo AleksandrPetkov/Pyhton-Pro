6dc4961d29fd2eda268d91a3faf3d8a8ba82ba23
Oleksandr Petkov <sasha111108@gmail.com>
2022-12-18 11:14:39 +0200

    Solution for hw 1 lesson 4. Program to get order cost(code in one line).

--- lesson_4_hw_1/task_1.py
+++ lesson_4_hw_1/task_1.py
@@ -1,1 +1,1 @@
-get_order_cost = lambda data: list(map(tuple, [[x[0], round(x[-1] * x[-2] + 10, 2)] if x[-1] * x[-2] < 100 else [x[0], round(x[-1] * x[-2], 2)] for x in data]))
+order = list(map(lambda x: (x[0], round(x[-1] * x[-2] + 10, 2)) if x[-1] * x[-2] < 100 else (x[0], round(x[-1] * x[-2], 2)), data))
