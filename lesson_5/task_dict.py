31d838a6a682a0c9c99700c376018460612dd195
Oleksandr Petkov <sasha111108@gmail.com>
2022-12-22 11:06:49 +0200

    Функция для вывода наиболее повтрояющегося слова в строке.

--- lesson_5/task_dict.py
+++ lesson_5/task_dict.py
@@ -1,7 +1,3 @@
-import re
-from collections import Counter
-
-
 def get_repeated_word(string):
     """
     Функция для вывода наиболее повторяющегося слова в строке.
@@ -9,8 +5,15 @@ def get_repeated_word(string):
     :return: возвращает самое повторяемое слово,
              если слов несколько, возвращает последнее.
     """
-    words = re.findall(r'\w+', string)
-    repeat_dict = Counter(words)
+    new_string = ''
+    punctuation = '''!()-[]{};?@#$%:'"\,./^&;*_,'''
+    for char in string:
+        if char in punctuation:
+            new_string += ''
+        else:
+            new_string += char
+    split_new_string = new_string.lower().split()
+    repeat_dict = {k: split_new_string.count(k) for k in split_new_string}
     max_val = max(repeat_dict.values())
     final_dict = {k: v for k, v in repeat_dict.items() if v == max_val}
     if len(final_dict) > 1:
