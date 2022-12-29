2773b77355f57930ca2991b05e7ce498bc682121
Oleksandr Petkov <sasha111108@gmail.com>
2022-12-29 16:11:28 +0200

    Функция преобразования веса и роста

--- lesson_7/utils.py
+++ lesson_7/utils.py
@@ -6,8 +6,8 @@ def convert_data(data_str):
     """
     data_list = data_str.replace('\n', ',').split(',')[3:-2]
     total_students = len(data_list) / 3
-    total_height = sum([float(data_list[idx]) for idx in range(1, len(data_list), 3) if data_list[idx]]) * 2.54
-    total_weight = sum([float(data_list[idx]) for idx in range(2, len(data_list), 3) if data_list[idx]]) * 0.453592
+    total_height = sum([float(data_list[idx]) for idx in range(1, len(data_list), 3)]) * 2.54
+    total_weight = sum([float(data_list[idx]) for idx in range(2, len(data_list), 3)]) * 0.453592
     avr_height = round(total_height / total_students, 2)
     avr_weight = round(total_weight / total_students, 2)
     return f'Average height is {avr_height} cm, Average weight is {avr_weight} kg'
